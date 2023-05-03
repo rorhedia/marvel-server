c83d8102eb73ac247b94b10d41c622d9c8747420
Daniel Heredia <rorhedia@gmail.com>
2023-05-02 21:24:02 -0600

    Creación de servicio para busquedas

--- routes/comics.route.js
+++ routes/comics.route.js
@@ -8,8 +8,22 @@ function comics(app) {
   app.use('/api/v1/comics', router);
 
   router.get('/', async (req, res) => {
-    const result = await ComicsAPI.getAll();
-    return res.json(result.data);
+    try {
+      const result = await ComicsAPI.getAll();
+      return res.json(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  router.post('/search', async (req, res) => {
+    try {
+      const { title, offset } = req.body;
+      const result = await ComicsAPI.getByTitle(title, offset);
+      return res.json(result.data);
+    } catch (e) {
+      console.log(e);
+    }
   });
 
 }
--- services/comics.service.js
+++ services/comics.service.js
@@ -3,14 +3,23 @@ const { MARVEL_API, MARVEL_PUBLIC_KEY, MARVEL_HASH } = require('./../config');
 
 class Comics {
 
-  #queryParams = `apikey=${MARVEL_PUBLIC_KEY}&ts=1&hash=${MARVEL_HASH}&orderBy=title`;
+  #queryParams = `apikey=${MARVEL_PUBLIC_KEY}&ts=1&hash=${MARVEL_HASH}&orderBy=title&limit=15`;
 
-  async getAll() {
+  async getAll(offset = 0) {
     try {
-      const url = `${MARVEL_API}/comics?${this.#queryParams}`;
+      const url = `${MARVEL_API}/comics?${this.#queryParams}&offset=${offset}`;
       return await axios.get(url);
     } catch (e) {
-      console.log(e);
+      throw new Error(e);
+    }
+  }
+
+  async getByTitle(title, offset) {
+    try {
+      const url = `${MARVEL_API}/comics?${this.#queryParams}&title=${title}&offset=${offset}`;
+      return await axios.get(url);
+    } catch (e) {
+      throw new Error(e);
     }
   }
 
